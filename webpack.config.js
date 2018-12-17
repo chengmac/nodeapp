141a8a40c6f18fa65339fa7a410cba027a05758f
chengmac <gd2216866233@163..com>
2018-12-17 23:37:11 +0800

    add Dockerfile

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ let webpack = require('webpack');
 let path = require('path');
 
 module.exports = {
-    entry: path.join(__dirname,'app.js'),
+    entry: path.join(__dirname,'./src/app.js'),
     output: {
         path: path.join(__dirname,'./public'),
         filename: 'js/index.js'
